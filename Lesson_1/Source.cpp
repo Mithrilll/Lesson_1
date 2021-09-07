29360fb531d0a52dea96d3af58b83542869a5b95
Mifril <rybalovalex03@gmail.com>
2021-09-07 11:53:24 +0700

    New sum function

--- Lesson_1/Source.cpp
+++ Lesson_1/Source.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
+int sum(int a, int b)
+{
+	return a + b;
+}
+
 int main()
 {
 	std::cout << "Hello world!" << std::endl;
+
+	std::cout << sum(5, 5) << std::endl;
 
 	return 0;
 }
